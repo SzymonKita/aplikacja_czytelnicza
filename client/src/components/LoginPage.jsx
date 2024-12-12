0f1e42ca1819748144de2aeb970cae5deac6c672
Reflux121 <jakub.ki@wp.pl>
2024-12-13 00:44:54 +0100

    Fixed login and register endpoint

--- client/src/components/LoginPage.jsx
+++ client/src/components/LoginPage.jsx
@@ -11,7 +11,7 @@ const LoginPage = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetch('http://localhost:5000/login', {
+    const response = await fetch('http://localhost:5000/auth/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
